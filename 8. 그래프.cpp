070caaafee6e0ea17f28c9c9652c15a83a4380c7
interviewsangu <59389967+interviewsangu@users.noreply.github.com>
2020-01-12 20:08:43 +0900

    Update 8. 그래프.cpp

--- 8. 그래프.cpp
+++ 8. 그래프.cpp
@@ -146,3 +146,201 @@ int main(){
   }
   
 1260번 : https://www.acmicpc.net/problem/1260
+- 인접 리스트 사용, 재귀
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+vector<int> a[1001];
+bool check[1001];
+void dfs(int node) {
+	check[node] = true;
+	printf("%d ", node);
+	for (int i = 0; i < a[node].size(); i++) {
+		int next = a[node][i];
+		if (check[next] == false) {
+			dfs(next);
+		}
+	}
+}
+
+void bfs(int start) {
+	queue<int> q;
+	memset(check, false, sizeof(check));
+	check[start] = true;
+	q.push(start);
+	while (!q.empty()) {
+		int node = q.front();
+		q.pop();
+		printf("%d ", node);
+		for (int i = 0; i < a[node].size(); i++) {
+			int next = a[node][i];
+			if (check[next] == false) {
+				check[next] = true;
+				q.push(next);
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m, start;
+	scanf("%d %d %d", &n, &m, &start);
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		a[u].push_back(v);
+		a[v].push_back(u);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(a[i].begin(), a[i].end());
+	}
+	dfs(start);
+	puts("");
+	bfs(start);
+	puts("");
+	return 0;
+}
+
+- 비재귀 구현
+#include <cstdio>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <queue>
+using namespace std;
+vector<int> a[1001];
+bool check[1001];
+void dfs(int node) {
+    stack<pair<int, int>> s;
+    s.push(make_pair(node, 0));
+    check[node] = true;
+    printf("%d ", node);
+    while (!s.empty()) {
+        int node = s.top().first;
+        int start = s.top().second;
+        s.pop();
+        for (int i = start; i < a[node].size(); i++) {
+            int next = a[node][i];
+            if (check[next] == false) {
+                printf("%d ", next);
+                check[next] = true;
+                s.push(make_pair(node, i + 1));
+                s.push(make_pair(next, 0));
+                break;
+            }
+        }
+    }
+}
+void bfs(int start) {
+    queue<int> q;
+    memset(check, false, sizeof(check));
+    check[start] = true;
+    q.push(start);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        printf("%d ", node);
+        for (int i = 0; i < a[node].size(); i++) {
+            int next = a[node][i];
+            if (check[next] == false) {
+                check[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
+int main() {
+    int n, m, start;
+    scanf("%d %d %d", &n, &m, &start);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++) {
+        sort(a[i].begin(), a[i].end());
+    }
+    dfs(start);
+    puts("");
+    bfs(start);
+    puts("");
+    return 0;
+}
+
+- 간선 구현
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+using namespace std;
+struct Edge {
+    int from, to;
+};
+Edge edge[20001];
+int cnt[1001];
+bool check[1001];
+bool cmp(const Edge &u, const Edge &v) {
+    if (u.from == v.from) {
+        return u.to < v.to;
+    } else {
+        return u.from < v.from;
+    }
+}
+void dfs(int node) {
+    check[node] = true;
+    printf("%d ",node);
+    for (int i=cnt[node-1]; i<cnt[node]; i++) {
+        int next = edge[i].to;
+        if (check[next] == false) {
+            dfs(next);
+        }
+    }
+}
+void bfs(int start) {
+    queue<int> q;
+    q.push(start);
+    check[start] = true;
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        printf("%d ",node);
+        for (int i=cnt[node-1]; i<cnt[node]; i++) {
+            int next = edge[i].to;
+            if (check[next] == false) {
+                check[next] = true;
+                q.push(next);
+            }
+        }
+    }
+}
+int main() {
+    int n, m, start;
+    scanf("%d %d %d",&n,&m,&start);
+    for (int i=0; i<m; i++) {
+        scanf("%d %d",&edge[i].from, &edge[i].to);
+        edge[i+m].from = edge[i].to;
+        edge[i+m].to = edge[i].from;
+    }
+    m *= 2;
+    sort(edge, edge+m, cmp);
+    for (int i=0; i<m; i++) {
+        cnt[edge[i].from] += 1;
+    }
+    for (int i=1; i<=n; i++) {
+        cnt[i] += cnt[i-1];
+    }
+    dfs(start);
+    printf("\n");
+    memset(check,false,sizeof(check));
+    bfs(start);
+    printf("\n");
+    return 0;
+}
+
+    - 위에서 핵심 : 총 개수 엣지 * 2, 들어오는 간선의 개수 세주기, 누적 합 해주기
+                   , cnt[node -1] ~ cnt[node]-1 까지 돌려주기
