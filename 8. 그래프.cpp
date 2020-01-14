3067b6941629dbd096df9d3c1fa9b1ad37182620
interviewsangu <59389967+interviewsangu@users.noreply.github.com>
2020-01-15 00:58:32 +0900

    Update 8. 그래프.cpp

--- 8. 그래프.cpp
+++ 8. 그래프.cpp
@@ -906,3 +906,100 @@ int main() {
     return 0;
 }
 
+2178번 : https://www.acmicpc.net/problem/2178
+
+#include <cstdio>
+#include <queue>
+using namespace std;
+int n, m;
+int a[100][100];
+int dist_and_check[100][100];
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%1d", &a[i][j]);
+		}
+	}
+	queue<pair<int, int>> q;
+	q.push(make_pair(0, 0));
+	dist_and_check[0][0] = 1;
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for (int k = 0; k < 4; k++) {
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+				if (dist_and_check[nx][ny] == 0 && a[nx][ny] == 1) {
+					q.push(make_pair(nx, ny));
+					dist_and_check[nx][ny] = dist_and_check[x][y] + 1;
+				}
+			}
+		}
+	}
+	printf("%d\n", dist_and_check[n-1][m-1]);
+	return 0;
+}
+
+7576번 : https://www.acmicpc.net/problem/7576
+
+#include <cstdio>
+#include <queue>
+using namespace std;
+int a[1000][1000];
+int d[1000][1000];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int main() {
+    int n,m;
+    scanf("%d %d",&m,&n);
+    queue<pair<int,int>> q;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            scanf("%d",&a[i][j]);
+            d[i][j] = -1;
+            if (a[i][j] == 1) {
+                q.push(make_pair(i,j));
+                d[i][j] = 0;
+            }
+        }
+    }
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int k=0; k<4; k++) {
+            int nx = x+dx[k];
+            int ny = y+dy[k];
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                if (a[nx][ny] == 0 && d[nx][ny] == -1) {
+                    d[nx][ny] = d[x][y] + 1;
+                    q.push(make_pair(nx,ny));
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (ans < d[i][j]) {
+                ans = d[i][j];
+            }
+        }
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            if (a[i][j] == 0 && d[i][j] == -1) {
+                ans = -1;
+            }
+        }
+    }
+
+    printf("%d\n",ans);
+    return 0;
+}
